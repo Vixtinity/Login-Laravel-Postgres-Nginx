8bc09b218aeab9daaba788341929bafe90b2786e
Vixtinity <08062006ismael@gmail.com>
2025-05-08 10:13:27 +0200

    primer commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM php:8.2-fpm
+
+# Establecer shell en modo de depuración
+RUN set -ex \
+    && apt-get update && apt-get install -y libpq-dev nodejs yarn \
+    && docker-php-ext-install pdo pdo_pgsql
+
+
+# Instalar Composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Establecer directorio de trabajo
+WORKDIR /var/www/html
+
+CMD ["php", "artisan", "serve", "--host=0.0.0.0"]
---
+++ Laravel-Login/app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+            'loginemail' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if(auth()->attempt(['name' => $incomingFields['loginame'], 'password' => $incormingFields['loginpassword ']])){
+            $request->session()->regenerate();
+
+        }
+
+    }
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'name' => ['required', 'min:3', 'max:10', Rule::unique('users', 'name')],
+            'email' => ['required', 'email', Rule::unique('users', 'name')],
+            'password' => ['required', 'min:8', 'max:200']
+        ]);
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/');
+    }
+}
