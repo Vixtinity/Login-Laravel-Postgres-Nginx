1a5d367a3d1211ed8b5f50e1cb9834206ac08e84
Vixtinity <08062006ismael@gmail.com>
2025-05-19 12:24:28 +0200

    ??

--- Laravel-Login/resources/views/register.blade.php
+++ Laravel-Login/resources/views/register.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @else
     @endif
