646cd71ad05c1b2540e03c483274024e2e3a2f49
Vixtinity <08062006ismael@gmail.com>
2025-05-19 10:14:26 +0200

    cambio y tal

--- Laravel-Login/resources/views/home.blade.php
+++ Laravel-Login/resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="{{ asset('css/home.css') }}" rel="stylesheet">
     <title>Iniciar sesión</title>
 </head>
 <body>
